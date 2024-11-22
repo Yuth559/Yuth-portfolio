2b0b217895a0c820037eb354bd2fc4ff2d08c10d
Yuth559 <151505320+Yuth559@users.noreply.github.com>
2024-11-22 13:49:38 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// Theme Toggle
+const themeToggle = document.querySelector('.theme-toggle');
+themeToggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  themeToggle.textContent = document.body.classList.contains('dark-mode') ? '🌙' : '☀️';
+});
+// JavaScript for Hamburger Menu
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
